6dcaba8fddab88805753a89add41e267ea28e3e0
KrishnaManogaran <kman0030@student.monash.edu>
2023-05-25 22:24:48 +1000

    completed task 3

--- threedeebeetree.py
+++ threedeebeetree.py
@@ -5,9 +5,9 @@ from dataclasses import dataclass, field
 I = TypeVar('I')
 Point = Tuple[int, int, int]
 
+
 @dataclass
 class BeeNode:
-
     key: Point
     item: I
     x_y_z: BeeNode | None = None
@@ -21,8 +21,24 @@ class BeeNode:
     subtree_size: int = 1
 
     def get_child_for_key(self, point: Point) -> BeeNode | None:
-
-        raise NotImplementedError()
+        if point[0] > self.key[0] and point[1] > self.key[1] and point[2] > self.key[2]:
+            return self.x_y_z
+        elif point[0] > self.key[0] and point[1] > self.key[1] and point[2] < self.key[2]:
+            return self.x_y_nz
+        elif point[0] > self.key[0] and point[1] < self.key[1] and point[2] > self.key[2]:
+            return self.x_ny_z
+        elif point[0] > self.key[0] and point[1] < self.key[1] and point[2] < self.key[2]:
+            return self.x_ny_nz
+        elif point[0] < self.key[0] and point[1] > self.key[1] and point[2] > self.key[2]:
+            return self.nx_y_z
+        elif point[0] < self.key[0] and point[1] < self.key[1] and point[2] > self.key[2]:
+            return self.nx_ny_z
+        elif point[0] < self.key[0] and point[1] > self.key[1] and point[2] < self.key[2]:
+            return self.nx_y_nz
+        elif point[0] < self.key[0] and point[1] < self.key[1] and point[2] < self.key[2]:
+            return self.nx_ny_nz
+        else:
+            return None
 
 
 class ThreeDeeBeeTree(Generic[I]):
@@ -64,7 +80,29 @@ class ThreeDeeBeeTree(Generic[I]):
         return node.item
 
     def get_tree_node_by_key(self, key: Point) -> BeeNode:
-        raise NotImplementedError()
+        return self.get_tree_node_by_key_aux(self.root, key)
+
+    def get_tree_node_by_key_aux(self, current: BeeNode, key: Point) -> BeeNode:
+        if current is None:
+            raise KeyError('Key not found: {0}'.format(key))
+        elif key == current.key:
+            return current
+        elif key[0] > current.key[0] and key[1] > current.key[1] and key[2] > current.key[2]:
+            return self.get_tree_node_by_key_aux(current.x_y_z, key)
+        elif key[0] > current.key[0] and key[1] > current.key[1] and key[2] < current.key[2]:
+            return self.get_tree_node_by_key_aux(current.x_y_nz, key)
+        elif key[0] > current.key[0] and key[1] < current.key[1] and key[2] < current.key[2]:
+            return self.get_tree_node_by_key_aux(current.x_ny_nz, key)
+        elif key[0] < current.key[0] and key[1] < current.key[1] and key[2] < current.key[2]:
+            return self.get_tree_node_by_key_aux(current.nx_ny_nz, key)
+        elif key[0] > current.key[0] and key[1] < current.key[1] and key[2] > current.key[2]:
+            return self.get_tree_node_by_key_aux(current.x_ny_z, key)
+        elif key[0] < current.key[0] and key[1] < current.key[1] and key[2] > current.key[2]:
+            return self.get_tree_node_by_key_aux(current.nx_ny_z, key)
+        elif key[0] < current.key[0] and key[1] > current.key[1] and key[2] < current.key[2]:
+            return self.get_tree_node_by_key_aux(current.nx_y_nz, key)
+        else:
+            return self.get_tree_node_by_key_aux(current.nx_y_z, key)
 
     def __setitem__(self, key: Point, item: I) -> None:
         self.root = self.insert_aux(self.root, key, item)
@@ -106,7 +144,8 @@ class ThreeDeeBeeTree(Generic[I]):
 
     def is_leaf(self, current: BeeNode) -> bool:
         """ Simple check whether or not the node is a leaf. """
-        raise NotImplementedError()
+        return current.subtree_size == 1
+
 
 if __name__ == "__main__":
     tdbt = ThreeDeeBeeTree()
@@ -114,4 +153,4 @@ if __name__ == "__main__":
     tdbt[(1, 5, 2)] = "B"
     tdbt[(4, 3, 1)] = "C"
     tdbt[(5, 4, 0)] = "D"
-    print(tdbt.root.get_child_for_key((4, 3, 1)).subtree_size) # 2
+    print(tdbt.root.get_child_for_key((4, 3, 1)).subtree_size)  # 2
