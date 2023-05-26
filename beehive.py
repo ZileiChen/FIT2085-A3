2e47586ef73ab986b257318d5ab8b3ecfeb96a9b
KrishnaManogaran <kman0030@student.monash.edu>
2023-05-26 21:05:01 +1000

    finished task 6

--- beehive.py
+++ beehive.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from heap import MaxHeap
 
+
 @dataclass
 class Beehive:
     """A beehive has a position in 3d space, and some stats."""
@@ -13,16 +14,52 @@ class Beehive:
     nutrient_factor: int
     volume: int = 0
 
+    def __gt__(self, other):
+        if isinstance(other, Beehive):
+            if self.capacity <= self.volume:
+                self_val = self.capacity * self.nutrient_factor
+            else:
+                self_val = self.volume * self.nutrient_factor
+            if other.capacity <= other.volume:
+                other_val = other.capacity * other.nutrient_factor
+            else:
+                other_val = other.volume * other.nutrient_factor
+            return self_val > other_val
+
+    def __le__(self, other):
+        if isinstance(other, Beehive):
+            if self.capacity <= self.volume:
+                self_val = self.capacity * self.nutrient_factor
+            else:
+                self_val = self.volume * self.nutrient_factor
+            if other.capacity <= other.volume:
+                other_val = other.capacity * other.nutrient_factor
+            else:
+                other_val = other.volume * other.nutrient_factor
+            return self_val <= other_val
+
+
 class BeehiveSelector:
 
     def __init__(self, max_beehives: int):
-        raise NotImplementedError()
+        self.heap = MaxHeap(max_beehives)
 
     def set_all_beehives(self, hive_list: list[Beehive]):
-        raise NotImplementedError()
-    
+        if not self.heap.length == 0:
+            self.heap = self.__init__(len(hive_list))
+        for i in hive_list:
+            self.add_beehive(i)
+
     def add_beehive(self, hive: Beehive):
-        raise NotImplementedError()
-    
-    def harvest_best_beehive(self):
-        raise NotImplementedError()
+        self.heap.add(hive)
+
+    def harvest_best_beehive(self) -> float:
+        hive: Beehive = self.heap.get_max()
+        amount = min(hive.capacity, hive.volume)
+        nutrition = hive.nutrient_factor
+
+        if hive.volume - hive.capacity > 0:
+            hive.volume = hive.volume - hive.capacity
+            self.add_beehive(hive)
+
+        return amount*nutrition
