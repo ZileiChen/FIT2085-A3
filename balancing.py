54f2f8d272343da3ed6a7dbe45a5b23870c46044
zche0160 <chen.zilei@hotmail.com>
2023-05-28 19:37:27 +1000

    Updating complexity analysis

--- balancing.py
+++ balancing.py
@@ -5,7 +5,12 @@ from math import ceil
 from ratio import Percentiles
 from threedeebeetree import Point
 
+
 def make_ordering(my_coordinate_list: list[Point]) -> list[Point]:
+    """
+    This method recursively calls the make_ordering_aux method.
+    Complexity: O(N*log(N)*log(N))
+    """
     px = Percentiles()
     py = Percentiles()
     pz = Percentiles()
@@ -14,7 +19,32 @@ def make_ordering(my_coordinate_list: list[Point]) -> list[Point]:
     make_ordering_aux(my_coordinate_list, solution, px, py, pz)
     return solution
 
-def make_ordering_aux(subsection: list, solution: list, px, py, pz):
+
+def make_ordering_aux(subsection: list, solution: list, px: Percentiles, py: Percentiles, pz: Percentiles):
+    """
+    The algorithm for this method involves first checking if the current size of the subsection or number of points to
+    be added to this subtree is less than 9. If it is, then it is added to the solution using the list extend() method
+    which has a time complexity of O(N), but this method isn't always called since most of the time the list is very
+    large.
+    It then clears the Percentiles class trees which has a complexity of O(1)
+    The points in the subsection or subtree is then added to the Percentiles classes using their x, y, and z
+    coordinates as keys. Which has a complexity of O(N*log(N))
+    The ratio method follows, which has a complexity of O(log(N) + O) where O is the number of points returned.
+    Since the number of points returned is always 75% of N, the complexity can be written as O(log(N)+0.75N).
+    We then choose a root for this subtree which is done using the intersection method from sets. This has a complexity
+    of O(min(a, b, c)) where a, b and c are the sizes of these sets. Since the sizes of these sets is equal to 0.75N,
+    the complexity if O(0.75N).
+    We then sort the remaining points into 8 further subsections/subtrees which has a complexity of O(N).
+    We finally then call this method recursively.
+
+    Overall, the complexity for 1 iteration of this recursive algorithm is O(N+1+log(N)+log(N)+0.75N+0.75N+N)
+    Which simplifies to O(N*log(N)+log(N)+N)
+    By considering a large input and the overall complexities of this algorithm, this recursive algorithm divides into
+    8 separate smaller algorithms. Hence, the recursion runs logarithmically O(log(N)) in terms of how many times it is
+    called for any given input.
+    Thus, the final complexity is given as O((N*log(N)+log(N)+N)*log(N)) = O(N*log(N)*log(N)) since the log-linear
+    complexity will dominate the time complexity as N increases.
+    """
     if len(subsection) <= 9:
         solution.extend(subsection)
         return
@@ -72,136 +102,4 @@ def make_ordering_aux(subsection: list, solution: list, px, py, pz):
     make_ordering_aux(npn, solution, px, py, pz)
     make_ordering_aux(nnp, solution, px, py, pz)
     make_ordering_aux(nnn, solution, px, py, pz)
-
-
-
-
-    # relative_factor_x = (100 - x_range[1] - x_range[0]) * 12.5 / 100
-    # relative_factor_y = (100 - y_range[1] - y_range[0]) * 12.5 / 100
-    # relative_factor_z = (100 - z_range[1] - z_range[0]) * 12.5 / 100
-    #
-    # x_candidate = x.ratio(x_range[0], x_range[1])
-    # y_candidate = y.ratio(y_range[0], y_range[1])
-    # z_candidate = z.ratio(z_range[0], z_range[1])
-    #
-    # # if len(x_candidate) <= 17:
-    # #     for coord in x_candidate:
-    # #         solution_list.append(coord)
-    # #         x.remove_point(coord[0])
-    # #         y.remove_point(coord[1])
-    # #         z.remove_point(coord[2])
-    # #     return
-    # # elif len(y_candidate) <= 17:
-    # #     for coord in y_candidate:
-    # #         solution_list.append(coord)
-    # #         x.remove_point(coord[0])
-    # #         y.remove_point(coord[1])
-    # #         z.remove_point(coord[2])
-    # #     return
-    # # elif len(z_candidate) <= 17:
-    # #     for coord in z_candidate:
-    # #         solution_list.append(coord)
-    # #         x.remove_point(coord[0])
-    # #         y.remove_point(coord[1])
-    # #         z.remove_point(coord[2])
-    # #     return
-    # # else:
-    # #     x_candidate = x_candidate[ceil(12.5/100 * len(x_candidate)) + 0 : len(x_candidate) - ceil(12.5/100 * len(x_candidate))]
-    # #     y_candidate = y_candidate[ceil(12.5/100 * len(y_candidate)) + 0 : len(y_candidate) - ceil(12.5/100 * len(y_candidate))]
-    # #     z_candidate = z_candidate[ceil(12.5/100 * len(z_candidate)) + 0 : len(z_candidate) - ceil(12.5/100 * len(z_candidate))]
-    #
-    # if max(len(x_candidate), len(y_candidate), len(z_candidate)) <= 17:
-    #     #add the rest of the points to the solution list in any order
-    #     for coord in x_candidate:
-    #         solution_list.append(coord)
-    #         # x.remove_point(coord[0])
-    #         # y.remove_point(coord[1])
-    #         # z.remove_point(coord[2])
-    #     for coord in y_candidate:
-    #         if not solution_list.__contains__(coord):
-    #             solution_list.append(coord)
-    #             # x.remove_point(coord[0])
-    #             # y.remove_point(coord[1])
-    #             # z.remove_point(coord[2])
-    #     for coord in z_candidate:
-    #         if not solution_list.__contains__(coord):
-    #             solution_list.append(coord)
-    #             # x.remove_point(coord[0])
-    #             # y.remove_point(coord[1])
-    #             # z.remove_point(coord[2])
-    #     return
-    # else:
-    #     x_candidate = x_candidate[
-    #               ceil(12.5 / 100 * len(x_candidate)) + 0: len(x_candidate) - ceil(12.5 / 100 * len(x_candidate))]
-    #     y_candidate = y_candidate[
-    #               ceil(12.5 / 100 * len(y_candidate)) + 0: len(y_candidate) - ceil(12.5 / 100 * len(y_candidate))]
-    #     z_candidate = z_candidate[
-    #               ceil(12.5 / 100 * len(z_candidate)) + 0: len(z_candidate) - ceil(12.5 / 100 * len(z_candidate))]
-    #
-    #
-    #
-    # candidates = list(set(x_candidate).intersection(y_candidate).intersection(z_candidate))
-    #
-    # median = len(candidates) // 2
-    #
-    # if len(candidates) == 0:
-    #     return
-    #
-    # x_index = x_sorted.index(candidates[median])
-    # y_index = y_sorted.index(candidates[median])
-    # z_index = z_sorted.index(candidates[median])
-    # solution_list.append(candidates[median])
-    #
-    # x.remove_point(candidates[median][0])
-    # y.remove_point(candidates[median][1])
-    # z.remove_point(candidates[median][2])
-    #
-    # # +x, -y, -z
-    # x_range = [x_index * 100 / len(x_sorted), x_range[1]]
-    # y_range = [y_range[0], 100 - (y_index * 100 / len(y_sorted))]
-    # z_range = [z_range[0], 100 - (z_index * 100 / len(z_sorted))]
-    # make_ordering_aux(solution_list, x, y, z, x_range, y_range, z_range, x_sorted, y_sorted, z_sorted)
-    #
-    # # -x, -y, +z
-    # x_range = [x_range[0], 100 - (x_index * 100 / len(x_sorted))]
-    # y_range = [y_range[0], 100 - (y_index * 100 / len(y_sorted))]
-    # z_range = [z_index * 100 / len(z_sorted), z_range[1]]
-    # make_ordering_aux(solution_list, x, y, z, x_range, y_range, z_range, x_sorted, y_sorted, z_sorted)
-    #
-    # # -x, +y, -z
-    # x_range = [x_range[0], 100 - (x_index * 100 / len(x_sorted))]
-    # y_range = [y_index * 100 / len(y_sorted), y_range[1]]
-    # z_range = [z_range[0], 100 - (z_index * 100 / len(z_sorted))]
-    # make_ordering_aux(solution_list, x, y, z, x_range, y_range, z_range, x_sorted, y_sorted, z_sorted)
-    #
-    # # -x, -y, -z
-    # x_range = [x_range[0], 100 - (x_index * 100 / len(x_sorted))]
-    # y_range = [y_range[0], 100 - (y_index * 100 / len(y_sorted))]
-    # z_range = [z_range[0], 100 - (z_index * 100 / len(z_sorted))]
-    # make_ordering_aux(solution_list, x, y, z, x_range, y_range, z_range, x_sorted, y_sorted, z_sorted)
-    #
-    #
-    # # +x, +y, +z
-    # x_range = [x_index * 100 / len(x_sorted), x_range[1]]
-    # y_range = [y_index * 100 / len(y_sorted), y_range[1]]
-    # z_range = [z_index * 100 / len(z_sorted), z_range[1]]
-    # make_ordering_aux(solution_list, x, y, z, x_range, y_range, z_range, x_sorted, y_sorted, z_sorted)
-    #
-    # # +x, +y, -z
-    # x_range = [x_index * 100 / len(x_sorted), x_range[1]]
-    # y_range = [y_index * 100 / len(y_sorted), y_range[1]]
-    # z_range = [z_range[0], 100 - (z_index * 100 / len(z_sorted))]
-    # make_ordering_aux(solution_list, x, y, z, x_range, y_range, z_range, x_sorted, y_sorted, z_sorted)
-    #
-    # # +x, -y, +z
-    # x_range = [x_index * 100 / len(x_sorted), x_range[1]]
-    # y_range = [y_range[0], 100 - (y_index * 100 / len(y_sorted))]
-    # z_range = [z_index * 100 / len(z_sorted), z_range[1]]
-    # make_ordering_aux(solution_list, x, y, z, x_range, y_range, z_range, x_sorted, y_sorted, z_sorted)
-    #
-    # # -x, +y, +z
-    # x_range = [x_range[0], 100 - (x_index * 100 / len(x_sorted))]
-    # y_range = [y_index * 100 / len(y_sorted), y_range[1]]
-    # z_range = [z_index * 100 / len(z_sorted), z_range[1]]
-    # make_ordering_aux(solution_list, x, y, z, x_range, y_range, z_range, x_sorted, y_sorted, z_sorted)
 
--- ratio.py
+++ ratio.py
@@ -23,18 +23,29 @@ class Percentiles(Generic[T]):
         :complexity: Worst case: O(N) where n is the number of elements in the search tree and the tree has a depth
         of N.
         Best case: O(logN) where N is the number of elements in the search tree and the tree is balanced.
+        However, it is assumed that all BST are bounded by a depth of log(n), the assumed worst case complexity is
+        O(log(N)).
         """
         # Using binary search tree to insert
         self.tree[item] = item
     
     def remove_point(self, item: T):
         """
-
+        Complexity: Same as add_point() method above.
         """
         # Using binary search tree to remove
         del self.tree[item]
 
     def ratio(self, x: int, y: int) -> list:
+        """
+        Complexity:
+        The kth_smallest() method has a complexity of O(log(N)) where N is the number of nodes in the tree.
+        The modified_inorder_traversal method is a recursive method that traverses the binary search tree in Inorder
+        traversal, starting from the xth smallest element to the yth smallest element. Hence, the only nodes that are
+        visited are the ones that we need as output. This leads to a complexity O(O) where O is the number of nodes
+        outputted by the function.
+        Hence, overall complexity of this function is O(log(N) + O)
+        """
         x_index = ceil(x/100 * len(self.tree)) + 1
         y_index = len(self.tree) - ceil(y/100 * len(self.tree))
         xth_element = self.tree.kth_smallest(x_index, self.tree.root).key
@@ -44,6 +55,11 @@ class Percentiles(Generic[T]):
         return result
 
     def modified_inorder_traversal(self, current_node: TreeNode, x: int, y: int, inorder_list: list):
+        """
+        This method traverses the binary search tree in Inorder traversal. However, it starts the traversal at the
+        k1th smallest element and finishes at the k2th smallest element. Therefore, the complexity is O(O) where O is the
+        number of nodes returned and traversed by the function.
+        """
         if current_node is not None:
             if x < current_node.key:
                 self.modified_inorder_traversal(current_node.left, x, y, inorder_list)
@@ -53,9 +69,17 @@ class Percentiles(Generic[T]):
                 self.modified_inorder_traversal(current_node.right, x, y, inorder_list)
 
     def add_point_key_value(self, key: T, value: T):
+        """
+        Modified version of add_point, which allows for adding an item with a key pair.
+        Complexity O(1)
+        """
         self.tree[key] = value
 
     def clear(self):
+        """
+        Removes every element in the tree.
+        Complexity O(1)
+        """
         self.tree.clear()
 
 
